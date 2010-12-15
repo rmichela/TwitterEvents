73cc520e2d31e0e52b2ab595e8eb0d890cea77e6
rmichela <deltahat@gmail.com>
2010-12-15 02:49:59 -0500

    removed jar loading hack. replaced with manifest file

--- src/TEPlugin.java
+++ src/TEPlugin.java
@@ -13,7 +13,6 @@
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,44 +31,38 @@ public class TEPlugin extends SuperPlugin {
 	public void enableExtra() {
 		
 		log.info("Starting TwitterEvents");
+			
+		String accessToken = config.getAccessToken();
+		String accessTokenSecret = config.getAccessTokenSecret();
 		
-		try {
-			ClassPathHack.addFile("twitter4j-core-2.1.9-SNAPSHOT.jar");
+		if(accessToken != config.pleaseRegister && accessTokenSecret != config.pleaseRegister) {
+			TEPluginListener listener = new TEPluginListener(config, log);
 			
-			String accessToken = config.getAccessToken();
-			String accessTokenSecret = config.getAccessTokenSecret();
-			
-			if(accessToken != config.pleaseRegister && accessTokenSecret != config.pleaseRegister) {
-				TEPluginListener listener = new TEPluginListener(config, log);
-				
-				// Register hMod hooks
-				if(config.getTweetLogin()) {
-					prls.add(etc.getLoader().addListener(PluginLoader.Hook.LOGIN, listener, this, PluginListener.Priority.LOW));
-					log.info("* Tweeting logins");
-				}
-				if(config.getTweetDisconnect()) {
-					prls.add(etc.getLoader().addListener(PluginLoader.Hook.DISCONNECT, listener, this, PluginListener.Priority.LOW));
-					log.info("* Tweeting disconnects");
-				}
-				if(config.getTweetBan()) {
-					prls.add(etc.getLoader().addListener(PluginLoader.Hook.BAN, listener, this, PluginListener.Priority.LOW));
-					log.info("* Tweeting bans");
-				}
-				if(config.getTweetKick()) {
-					prls.add(etc.getLoader().addListener(PluginLoader.Hook.KICK, listener, this, PluginListener.Priority.LOW));
-					log.info("* Tweeting kicks");
-				}
-				if(config.getTweetDeath()) {
-					prls.add(etc.getLoader().addListener(PluginLoader.Hook.HEALTH_CHANGE, listener, this, PluginListener.Priority.LOW));
-					log.info("* Tweeting deaths");
-				}
-				
-				initMessagesConfig();
-			} else {
-				log.info("*** TwitterEvents is not yet registered to send tweets to Twitter! Consult the README.");
+			// Register hMod hooks
+			if(config.getTweetLogin()) {
+				prls.add(etc.getLoader().addListener(PluginLoader.Hook.LOGIN, listener, this, PluginListener.Priority.LOW));
+				log.info("* Tweeting logins");
 			}
-		} catch(IOException e) {
-			log.warning(e.getMessage());
+			if(config.getTweetDisconnect()) {
+				prls.add(etc.getLoader().addListener(PluginLoader.Hook.DISCONNECT, listener, this, PluginListener.Priority.LOW));
+				log.info("* Tweeting disconnects");
+			}
+			if(config.getTweetBan()) {
+				prls.add(etc.getLoader().addListener(PluginLoader.Hook.BAN, listener, this, PluginListener.Priority.LOW));
+				log.info("* Tweeting bans");
+			}
+			if(config.getTweetKick()) {
+				prls.add(etc.getLoader().addListener(PluginLoader.Hook.KICK, listener, this, PluginListener.Priority.LOW));
+				log.info("* Tweeting kicks");
+			}
+			if(config.getTweetDeath()) {
+				prls.add(etc.getLoader().addListener(PluginLoader.Hook.HEALTH_CHANGE, listener, this, PluginListener.Priority.LOW));
+				log.info("* Tweeting deaths");
+			}
+			
+			initMessagesConfig();
+		} else {
+			log.info("*** TwitterEvents is not yet registered to send tweets to Twitter! Consult the README.");
 		}
 	}
 
