94ef5b3c4b85fdb481567ee58b33d1ba8df08844
rmichela <deltahat@gmail.com>
2010-12-15 02:02:10 -0500

    version 0.1

--- src/Register.java
+++ src/Register.java
@@ -1,3 +1,23 @@
+//Copyright (C) 2010  Ryan Michela
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -5,10 +25,6 @@ import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 import twitter4j.http.AccessToken;
 import twitter4j.http.RequestToken;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 /**
  * Example application that uses OAuth method to acquire access to your account.<br>
@@ -31,11 +47,12 @@ public final class Register {
         	
         	
             Twitter twitter = new TwitterFactory(cb.build()).getInstance();
+            AccessToken accessToken = null;
             try {
                 // get request token.
                 // this will throw IllegalStateException if access token is already available
                 RequestToken requestToken = twitter.getOAuthRequestToken();
-                AccessToken accessToken = null;
+                
 
                 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
                 while (null == accessToken) {
@@ -69,7 +86,15 @@ public final class Register {
             }
             twitter.updateStatus("Hello from your Minecraft server");
             System.out.println("Successfully connected to Twitter.");
-            System.out.println("Add the access token and access token secret above to the TwitterEvents.config file to complete setup.");
+            
+            // Write the properties file
+            PrintWriter pw = new PrintWriter(new FileWriter("TwitterEvents.properties"));
+            pw.println("accessToken=" + accessToken.getToken());
+            pw.println("accessTokenSecret=" + accessToken.getTokenSecret());
+            pw.close();
+            
+            
+            System.out.println("Your TwitterEvents.properties file has been created with your access tokens.");
             System.exit(0);
         } catch (TwitterException te) {
             System.out.println("Failed to get timeline: " + te.getMessage());
